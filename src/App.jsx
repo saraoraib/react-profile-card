cdb8d480ed375845c9047822c0b3e19029e0b93f
saraoraib <saraoraib6@gmail.com>
2026-07-05 12:12:55 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HelloWorld from './HelloWorld';
+import ProfileCard from './ProfileCard';
+
+   function App() {
+     return (
+       <div>
+         <HelloWorld />
+         <ProfileCard />
+       </div>
+     );
+   }
+
+   export default App;
---
+++ src/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import './ProfileCard.css';
+
+   function ProfileCard() {
+     return (
+       <div className="profile-card">
+         <h2>Sara Oraib</h2>
+         <p>AI Engineer learning react:)</p>
+       </div>
+     );
+   }
+
+   export default ProfileCard;
